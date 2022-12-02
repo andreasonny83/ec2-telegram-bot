ea0daa9241cdfa8a68076197b5aa4596c8f41ef8
andreasonny83 <andreasonny83@gmail.com>
2022-12-02 22:33:25 +0000

    telegraf

---
+++ server.js
@@ -0,0 +1,87 @@
+const { Telegraf } = require("telegraf");
+
+require("dotenv").config();
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TOKEN;
+
+// Create a bot
+const bot = new Telegraf(token);
+
+const helpMessage = `
+Say something to me
+/start - start the bot
+/help - command reference
+/operations - start an operation
+`;
+
+bot.use((ctx, next) => {
+  if (ctx.message?.text) {
+    console.log(ctx.from.username + " said " + ctx.message.text);
+  }
+
+  next();
+});
+
+bot.start((ctx) => {
+  ctx.reply("Hi, I'm the Trading Bot");
+  ctx.reply(helpMessage);
+});
+
+bot.help((ctx) => {
+  ctx.reply(helpMessage);
+});
+
+bot.command("operations", (ctx) => {
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    "Please select an operation to perform",
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Buy",
+              callback_data: "buy",
+            },
+            {
+              text: "Sell",
+              callback_data: "sell",
+            },
+          ],
+          [
+            {
+              text: "Leverage",
+              callback_data: "leverage",
+            },
+            {
+              text: "Close",
+              callback_data: "close",
+            },
+          ],
+          [
+            {
+              text: "Position",
+              callback_data: "position",
+            },
+            {
+              text: "Done",
+              callback_data: "done",
+            },
+          ],
+        ],
+      },
+    }
+  );
+});
+
+bot.command("echo", (ctx) => {
+  const reply = ctx.message.text.split("/echo")[1];
+  ctx.reply(reply);
+});
+
+// bot.on("message", (ctx) => {
+//   console.warn(ctx);
+// });
+
+bot.launch();
